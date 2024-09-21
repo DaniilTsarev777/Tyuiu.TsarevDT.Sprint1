8f6e9ac963b6945a72503e60eb88f3b3a08186f4
DaniilTsarev777 <daniilwestsiberian2@gmail.com>
2024-09-21 21:50:53 +0500

    Add files via upload
    task0

---
+++ Tyuiu.TsarevDT.Sprint1.Task0.V11/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.TsarevDT.Sprint1.Task0.V11.Lib;
+namespace Tyuiu.TsarevDT.Sprint1.Task0.V11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Царёв Д. И. | СМАРТб-24-1";
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* Спринт #1                                                             *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                      *");
+            Console.WriteLine("* Задание #0                                                            *");
+            Console.WriteLine("* Варинат #11                                                           *");
+            Console.WriteLine("* Выполнил: Царёв Даниил Ильич | СМАРТб-24-1                            *");
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                              *");
+            Console.WriteLine("* Написать  программу, которая вычисляет выражение 4*5/2-18/2/3         *");
+            Console.WriteLine("* и печатает результат на экране.                                       *");
+            Console.WriteLine("*                                                                       *");
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* ИССХОДНЫЕ ДАННЫЕ:                                                     *");
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* 4*5/2-18/2/3                                                          *");
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                            *");
+            Console.WriteLine("*************************************************************************");
+
+            Console.WriteLine(ds.Calculate());
+            Console.ReadLine();
+        }
+    }
+}
