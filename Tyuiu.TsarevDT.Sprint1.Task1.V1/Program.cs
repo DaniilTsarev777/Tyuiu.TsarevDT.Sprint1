ad09ff3ad82c4136568c2b14f6340ba2b584131c
DaniilTsarev777 <daniilwestsiberian2@gmail.com>
2024-09-22 17:35:36 +0500

    Add files via upload
    Task1

---
+++ Tyuiu.TsarevDT.Sprint1.Task1.V1/Program.cs
@@ -0,0 +1,47 @@
+﻿using Tyuiu.TsarevDT.Sprint1.Task1.V1.Lib;
+namespace Tyuiu.TsarevDT.Sprint1.Task1.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Царёв Д. И. | СМАРТб-24-1";
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* Спринт #1                                                             *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                      *");
+            Console.WriteLine("* Задание #1                                                            *");
+            Console.WriteLine("* Варинат #1                                                            *");
+            Console.WriteLine("* Выполнил: Царёв Даниил Ильич | СМАРТб-24-1                            *");
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                              *");
+            Console.WriteLine("* Написать  программу, которая запрашивает у пользователя исходные      *");
+            Console.WriteLine("* данные, вычисляет результат по формуле x/3/y+6*a и печатает его       *");
+            Console.WriteLine("* на экране.                                                            *");
+            Console.WriteLine("*                                                                       *");
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* ИССХОДНЫЕ ДАННЫЕ:                                                     *");
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("*                                                                       *");
+
+
+            double a, x, y;
+            Console.WriteLine("Введите значение A:");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение X:");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение Y:");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                            *");
+            Console.WriteLine("*************************************************************************");
+
+            Console.WriteLine(ds.Calculate(a, x, y));
+            Console.ReadLine();
+        }
+    }
+}
