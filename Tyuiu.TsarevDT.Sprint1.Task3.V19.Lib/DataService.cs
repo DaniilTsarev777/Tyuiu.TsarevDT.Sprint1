988fb1e20302d8e900c830913adc99f4f5ed3a6d
DaniilTsarev777 <daniilwestsiberian2@gmail.com>
2024-09-26 17:55:26 +0500

    Add files via upload
    Task3

---
+++ Tyuiu.TsarevDT.Sprint1.Task3.V19.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.TsarevDT.Sprint1.Task3.V19.Lib
+{
+    public class DataService : ISprint1Task3V19
+    {
+        public bool Calculate(double x1, double y1, double x2, double y2)
+        {
+            return Math.Abs(x1 - x2) == Math.Abs(y1 - y2);
+        }
+
+        public void Calculate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
