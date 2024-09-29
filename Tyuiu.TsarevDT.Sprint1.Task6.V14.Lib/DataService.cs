a3e3d8561836d44e1ef21c6a02c8275270b5c44c
DaniilTsarev777 <daniilwestsiberian2@gmail.com>
2024-09-29 16:35:54 +0500

    Add files via upload
    Task6

---
+++ Tyuiu.TsarevDT.Sprint1.Task6.V14.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.TsarevDT.Sprint1.Task6.V14.Lib
+{
+    public class DataService : ISprint1Task6V14
+    {
+        public bool CheckLowerCaseRusLetters(string value)
+        {
+            return Regex.IsMatch(value, @"^[а-яё]+$");
+        }
+    }
+}
---
+++ Tyuiu.TsarevDT.Sprint1.Task6.V14/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.TsarevDT.Sprint1.Task6.V14.Lib;
+namespace Tyuiu.TsarevDT.Sprint1.Task6.V14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Царёв Д. И. | СМАРТб-24-1";
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* Спринт #1                                                             *");
+            Console.WriteLine("* Тема: Работа со строками класс String                                 *");
+            Console.WriteLine("* Задание #6                                                            *");
+            Console.WriteLine("* Варинат #14                                                           *");
+            Console.WriteLine("* Выполнил: Царёв Даниил Ильич | СМАРТб-24-1                            *");
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                              *");
+            Console.WriteLine("* пользователь вводит текст. Проверить, что строка составлена только из *");
+            Console.WriteLine("* строчных русских букв.                                                *");
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* ИССХОДНЫЕ ДАННЫЕ:                                                     *");
+            Console.WriteLine("*************************************************************************");
+            string x;
+            Console.WriteLine("Введите текст");
+            x = Console.ReadLine();
+
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                            *");
+            Console.WriteLine("*************************************************************************");
+
+            Console.WriteLine(ds.CheckLowerCaseRusLetters(x));
+            Console.ReadKey();
+        }
+    }
+}
